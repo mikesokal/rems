ddf96e93eb713f2699c0dd651f99c6cdccfa266e
Andy Teucher <andy.teucher@gov.bc.ca>
2018-06-15 10:46:16 -0700

    Use crul in ems_locations

--- R/ems_locations.R
+++ R/ems_locations.R
@@ -10,36 +10,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-construct_url <- function(obj, epsg, query) {
-  baseurl <- "https://openmaps.gov.bc.ca/geo/pub/{obj}/ows?service=WFS&version=2.0.0&request=GetFeature&typeName={obj}&SRSNAME=epsg:{epsg}&outputFormat=json{query}"
-
-  if (!is.null(query)) {
-    query <- paste0("&CQL_FILTER=", query)
-  } else {
-    query <- ""
-  }
-  URLencode(glue::glue(baseurl, obj = obj, epsg = epsg, query = query))
-}
-
-bcdc_map <- function(id, epsg = 3005, query = NULL, sf) {
-  url <- construct_url(id, epsg, query)
-  tmp <- tempfile(fileext = ".json")
-  on.exit(unlink(tmp))
-  res <- httr::GET(url, httr::write_disk(tmp))
-  httr::stop_for_status(res)
-  if (sf) {
-    return(sf::st_read(tmp, quiet = TRUE))
-  } else {
-    res <- jsonlite::fromJSON(tmp)
-    ret <- res$features$properties
-    attr(ret, "geometry_type") <- unique(res$features$geometry$type)
-    attr(ret, "crs") <- res$crs$properties$name
-    ret
-  }
-  # TODO: look at using crul for paging
-}
-
-
 #' Get EMS monitoring locations
 #'
 #' Get an `sf` object of EMS monitoring locations
@@ -61,6 +31,103 @@ ems_locations <- function(epsg = 3005, query = "LOCATION_TYPE_CD='21'", sf = TRU
   if (sf && !requireNamespace("sf", quietly = TRUE)) {
     stop("Package sf required", call. = FALSE)
   }
-  bcdc_map("WHSE_ENVIRONMENTAL_MONITORING.EMS_MONITORING_LOCN_TYPES_SVW",
-           query = query, sf = sf)
+  bcdc_map(ems_loc_object_name(), query = query, sf = sf,
+           sort = "MONITORING_LOCATION_ID")
+}
+
+#' Get the data field names and types for the EMS monitoring locations
+#'
+#' This can help you know what to query in [ems_locations()]
+#'
+#' @return A data.frame of column names and their properties
+#' @export
+ems_location_features <- function() {
+  cli <- make_client()
+
+  res <- cli$get(path = sprintf("geo/pub/%s/ows", ems_loc_object_name()),
+                 query = list(service = "WFS",
+                              version = "2.0.0",
+                              request = "describeFeatureType",
+                              typeName = ems_loc_object_name(),
+                              outputFormat = "application/json"))
+
+  res$raise_for_status()
+
+  jsonlite::fromJSON(res$parse())[["featureTypes"]][["properties"]][[1]]
+}
+
+bcdc_map <- function(id, epsg = 3005, query = NULL, sf, sort) {
+  cli <- make_client()
+
+  cli_pag <- crul::Paginator$new(cli,
+                                 by = "query_params",
+                                 limit_param = "count",
+                                 offset_param = "startIndex",
+                                 limit = 100000,
+                                 limit_chunk = 10000)
+
+  query_params <- list(service = "WFS",
+                       version = "2.0.0",
+                       request = "GetFeature",
+                       typeName = id,
+                       SRSNAME = sprintf("epsg:%s", epsg),
+                       outputFormat = "json",
+                       sortBy = sort,
+                       cql_filter = query)
+
+  cli_pag$get(path = sprintf("geo/pub/%s/ows", id),
+              query = Filter(Negate(is.null), query_params))
+
+  status <- cli_pag$status_code()
+
+  if (all(status >= 300)) {
+    stop("All API calls failed")
+  } else if (any(status >= 300)) {
+    warning("Some API calls failed")
+  }
+
+  parsed_response <- cli_pag$parse()
+
+  if (sf) {
+    parsed_response_to_sf(parsed_response[status < 300])
+  } else {
+    parsed_response_to_df(parsed_response[status < 300])
+  }
+}
+
+
+ems_loc_object_name <- function()
+  "WHSE_ENVIRONMENTAL_MONITORING.EMS_MONITORING_LOCN_TYPES_SVW"
+
+make_client <- function()
+  crul::HttpClient$new("https://openmaps.gov.bc.ca")
+
+parsed_response_to_sf <- function(x) {
+  ret_list <- lapply(x, safe_st_read)
+
+  to_bind <- vapply(ret_list, function(x) {
+    !is.null(x) && inherits(x, "sf") && nrow(x) > 0
+  }, FUN.VALUE = logical(1))
+
+  do.call(rbind, ret_list[to_bind])
+}
+
+safe_st_read <- function(x, ...) {
+  if (!requireNamespace("sf", queitly = TRUE)) {
+    stop("sf package required to return an sf object")
+  }
+  tryCatch(sf::st_read(x, stringsAsFactors = FALSE, quiet = TRUE, ...),
+           error = function(e) NULL)
+}
+
+parsed_response_to_df <- function(x) {
+  ret_list <- lapply(x, function(y) {
+    jsonlite::fromJSON(y)[["features"]][["properties"]]
+  })
+
+  to_bind <- vapply(ret_list, function(x) {
+    !is.null(x) && inherits(x, "data.frame") && nrow(x) > 0
+  }, FUN.VALUE = logical(1))
+
+  do.call(rbind, ret_list[to_bind])
 }
